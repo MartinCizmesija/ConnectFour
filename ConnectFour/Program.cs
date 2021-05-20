a36aafa443001a5cb8a18d9440ea33faeb5a3321
MartinCizmesija <martin.cizmesija@fer.hr>
2021-05-21 00:38:34 +0200

    Added victoy check

--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -5,6 +5,7 @@ namespace ConnectFour
 {
     class Program
     {
+        static int BOARDDEPTH = 6;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to connect four");
@@ -12,7 +13,7 @@ namespace ConnectFour
             Console.Write("Enter 1 if you want to play first, or 9 if you want computer to play first: ");
             string input = Console.ReadLine();
 
-            int[,] playingBoard = new int[7, 7];
+            int[,] playingBoard = new int[BOARDDEPTH, 7];
             int[] availableFields = new int[7];
             var rand = new Random();
             int playingColumnNumber;
@@ -34,10 +35,16 @@ namespace ConnectFour
 
             while (true)
             {
+                //computer turn
                 if (computerMove)
                 {
                     Console.WriteLine("Computer move");
+                    
                     playingColumnNumber = rand.Next(0, 6);
+                    while (availableFields[playingColumnNumber] == BOARDDEPTH) {
+                        playingColumnNumber = rand.Next(0, 6);
+                    }
+
                     playingBoard[availableFields[playingColumnNumber], playingColumnNumber] = 9;
 
                     Console.Write("|");
@@ -65,14 +72,33 @@ namespace ConnectFour
                     }
 
                     Console.WriteLine("");
+                    if (maxDepth == BOARDDEPTH) Console.WriteLine("|-----------------------------------------|");
+
+                    if (VictoryCalculation(playingBoard, computerMove))
+                    {
+                        Console.WriteLine("Computer won!");
+                        break;
+                    }
 
                     ++availableFields[playingColumnNumber];
-                    if(availableFields[playingColumnNumber] == maxDepth) ++maxDepth;
+                    if (availableFields[playingColumnNumber] == maxDepth && maxDepth < 6) ++maxDepth;
                     computerMove = false;
                 } else
+                //player turn
                 {
                     Console.Write("Enter column number you want to play (0-6): ");
                     playingColumnNumber = int.Parse(Console.ReadLine());
+                    while (playingColumnNumber > 6)
+                    {
+                        Console.Write("Please enter valid column number (0-6): ");
+                        playingColumnNumber = int.Parse(Console.ReadLine());
+                    }
+
+                    while (availableFields[playingColumnNumber] == BOARDDEPTH)
+                    {
+                        Console.Write("Column is full. Enter some other column number: ");
+                        playingColumnNumber = int.Parse(Console.ReadLine());
+                    }
 
                     playingBoard[availableFields[playingColumnNumber], playingColumnNumber] = 1;
 
@@ -101,14 +127,73 @@ namespace ConnectFour
                     }
 
                     Console.WriteLine("");
+                    if (maxDepth == BOARDDEPTH) Console.WriteLine("|-----------------------------------------|");
+
+                    if (VictoryCalculation(playingBoard, computerMove))
+                    {
+                        Console.WriteLine("You won!");
+                        break;
+                    }
 
                     ++availableFields[playingColumnNumber];
-                    if (availableFields[playingColumnNumber] == maxDepth) ++maxDepth;
+                    if (availableFields[playingColumnNumber] == maxDepth && maxDepth < 6) ++maxDepth;
                     computerMove = true;
                 }
             }
 
-
+            Console.WriteLine("Enter anything to close");
+            Console.ReadLine();
         }
+
+        static bool VictoryCalculation(int[,] playingBoard, bool computerTurn)
+        {
+            int wantedNumber;
+            if (computerTurn) wantedNumber = 9;
+            else wantedNumber = 1;
+
+            int horizontalVictoryCount = 0;
+
+            for (int a = 0; a < BOARDDEPTH; ++a)
+            {
+                for (int b = 0; b < 7; ++b)
+                {
+                    //horizontal check
+                    if (playingBoard[a, b] == wantedNumber) ++horizontalVictoryCount;
+                    else horizontalVictoryCount = 0;
+                    if (horizontalVictoryCount == 4) return true;
+
+                    if (a <= BOARDDEPTH - 4)
+                    {
+                        //vertical check
+                        if (playingBoard[a, b] == wantedNumber
+                            && playingBoard[a + 1, b] == wantedNumber
+                            && playingBoard[a + 2, b] == wantedNumber
+                            && playingBoard[a + 3, b] == wantedNumber) return true;
+
+                        //left diagonal check
+                        if (b <= 3)
+                        {
+                            if (playingBoard[a, b] == wantedNumber
+                                && playingBoard[a + 1, b + 1] == wantedNumber
+                                && playingBoard[a + 2, b + 2] == wantedNumber
+                                && playingBoard[a + 3, b + 3] == wantedNumber) return true;
+                        }
+
+                        //right diagonal check
+                        if (b >= 3)
+                        {
+                            if (playingBoard[a, b] == wantedNumber
+                                && playingBoard[a + 1, b - 1] == wantedNumber
+                                && playingBoard[a + 2, b - 2] == wantedNumber
+                                && playingBoard[a + 3, b - 3] == wantedNumber) return true;
+                        }
+                    }
+                }
+                horizontalVictoryCount = 0;
+            }
+
+            return false;
+        }
+
     }
 }
